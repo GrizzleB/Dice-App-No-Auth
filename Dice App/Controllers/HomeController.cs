624d282e855df33259dfae60e26690241b261e91
Ryan Gerhardt <rgerhardt2@my.stlcc.edu>
2020-10-05 23:29:31 -0500

    Add project files.

---
+++ Dice App/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dice_App.Models;
+
+namespace Dice_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Die die)
+        {
+            ViewBag.Dice = die.Roll();
+            return View(die);
+        }
+
+    }
+}
